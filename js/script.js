db74d9cef5f5d6f3f70bb641765c2a90003ef51a
Mai <mai.cmv2@gmail.com>
2026-06-22 13:19:19 -0300

    arreglo boton leer mas en noticias de deportes y completo formulario admin

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ const NOTICIAS_INICIALES = [
         titulo: "Lo ultimo de Basketball! ¿Las nuevas estrellas del Basketball creceran en Puerto Rico?",
         descripcion: "La primera NBA Basketball School se inaugura en Puerto Rico",
         imagen: "https://images.unsplash.com/photo-1546519638-68e109498ffc?fm=jpg&q=60&w=3000&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8YmFza2V0YmFsbCUyMHdhbGxwYXBlcnxlbnwwfHwwfHx8MA%3D%3D",
-        categoria: "deportes",
+        categoria: "deporte",
         destacada: false,
         link: "noticias/deporte/nba.html",
     },
@@ -11,7 +11,7 @@ const NOTICIAS_INICIALES = [
         titulo:"Así queda el Mundial de F1 2026 tras el GP de Mónaco: puntos y posiciones.",
         descripcion:"Las clasificaciones de pilotos y constructores del Mundial de Fórmula 1 2026 tras el Gran Premio de Mónaco, con Andrea Kimi Antonelli, italiano de 19 años, ampliando su ventaja al frente del campeonato.",
         imagen:"https://elsoldechiapas.com/wp-content/uploads/2026/06/LAS-INTELIGENCIAS-ARTIFICIALES-YA-ELIGIERON-%C2%BFQUIEN-SERA-EL-CAMPEON-DEL-MUNDIAL-2026-9-768x615.webp",
-        categoria: "deportes",
+        categoria: "deporte",
         destacada: true,
         link:"noticias/deporte/formulauno.html",
 
@@ -20,7 +20,7 @@ const NOTICIAS_INICIALES = [
         titulo: "Acertó los últimos cuatro campeones del mundo y ahora predijo quién levantará el trofeo en el Mundial de Fútbol 2026",
         descripcion:"La empresa de videojuegos deportivos simuló el torneo al igual que lo hizo en Sudáfrica 2010, Brasil 2014, Rusia 2018 y Qatar 2022",
         imagen:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTIS9Cde_6xJaGSYDeaUzKPHjb-HkVbVNdZYQ&s",
-        categoria:"deportes",
+        categoria:"deporte",
         destacada: false,
         link:"noticias/deporte/futbol.html",
     },
@@ -28,30 +28,32 @@ const NOTICIAS_INICIALES = [
         titulo:"Argentina en la VNL 2026: Previa, jugadores, calendario de partidos y cómo ver.",
         descripcion:"El equipo sudamericano buscará regresar a la fase final del torneo tras quedar eliminado en la primera ronda en 2025. Conoce el calendario, horarios y las claves para seguir a Argentina en la VNL 2026.",
         imagen:"https://www.ole.com.ar/2025/09/16/vSMDD1uJI_720x0__1.jpg",
-        categoria:"deportes",
+        categoria:"deporte",
         destacada: false,
-        link: "noticias/deportes/voleymasculino.html",
+        link: "noticias/deporte/voleymasculino.html",
     },
     {
         titulo:"Las Panteras vuelven a la acción: agenda y convocadas para los amistosos ante Bulgaria.",
         descripcion:"La Selección Argentina inicia su calendario internacional 2026 con dos encuentros en Santa Fe y Rosario. Aquí, un repaso del cronograma completo de la serie y la lista elegida por Facundo Morando.",
         imagen:"https://feva.org.ar/wp-content/uploads/2024/08/WhatsApp-Image-2024-08-25-at-22.45.57.jpeg",
-        categoria:"deportes",
+        categoria:"deporte",
         destacada: false,
-        link:"noticias/deportes/voleyfemenino.html",
+        link:"noticias/deporte/voleyfemenino.html",
     },
     {
         titulo:"Argentina se subió al podio en el Sudamericano de Remo de Brasil.",
         descripcion:"El seleccionado argentino obtuvo 20 medallas para quedarse con el tercer lugar del medallero en el Campeonato para las categorías U19, U23 y adaptadas que se realizó en Porto Alegre.",
         imagen:"https://www.argentina.gob.ar/sites/default/files/2026/03/hthdhbdfb.jpeg",
-        categoria:"deportes",
+        categoria:"deporte",
         destacada: false,
-        link:"noticias/deportes/remo.html",
+        link:"noticias/deporte/remo.html",
     }
 ];
 
 function cargarNoticiasIniciales() {
-    if (!localStorage.getItem("noticias")) {
+    const noticias = JSON.parse(localStorage.getItem("noticias"));
+
+    if (!noticias || noticias.length === 0) {
         localStorage.setItem("noticias", JSON.stringify(NOTICIAS_INICIALES));
     }
 }
@@ -86,6 +88,7 @@ function renderizarNoticias() {
 }
 
 function renderizarDeportes() {
+    console.log("Entró a renderizar deportes");
     const contenedor =
         document.getElementById("deportes-container");
     if (!contenedor) return;
@@ -94,7 +97,7 @@ function renderizarDeportes() {
         JSON.parse(localStorage.getItem("noticias")) || [];
     const deportes =
         noticias.filter(
-            noticia => noticia.categoria === "deportes"
+            noticia => noticia.categoria === "deporte"
         );
     deportes.forEach(function(noticia) {
         contenedor.innerHTML += `
@@ -102,7 +105,7 @@ function renderizarDeportes() {
                 <img src="${noticia.imagen}" alt="${noticia.titulo}">
                 <h3>${noticia.titulo}</h3>
                 <p>${noticia.descripcion}</p>
-                <a href="${noticia.enlace}" class="leerMas">
+                <a href="${noticia.link}" class="leerMas">
                     Leer más
                 </a>
             </article>
@@ -171,12 +174,6 @@ document.addEventListener("DOMContentLoaded", function() {
     if (!sessionStorage.getItem("browserSession")) {
         localStorage.removeItem("adminToken");
     }
-    cargarNoticiasIniciales();
-    //renderizarNoticias();
-    renderizarDeportes();
-    iniciarModoOscuro();
-    cargarCotizacionDolar();
-    iniciarBuscador();
 
     const botonInicio = document.querySelector(".botonInicio");
     if (botonInicio) {
@@ -186,3 +183,10 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     }
 });
+
+cargarNoticiasIniciales();
+//renderizarNoticias();
+renderizarDeportes();
+iniciarModoOscuro();
+cargarCotizacionDolar();
+iniciarBuscador();
